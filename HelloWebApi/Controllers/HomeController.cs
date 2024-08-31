19650756dba6bf0f7bd8d3fe42e8edd05ea13960
Melih <melihguvendev@gmail.com>
2024-09-01 00:59:21 +0300

    Add project files.

---
+++ HelloWebApi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using HelloWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebApi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            {
+                var result = new ResponseModel()
+                {
+                    HttpStatusCode = 200,
+                    Message = "Hello World!"
+                };
+
+                return Ok(result);
+            }
+           
+        }
+    }
+}
---
+++ HelloWebApi/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloWebApi.Models
+{
+    public class ResponseModel
+    {
+        public int HttpStatusCode { get; set; }
+        public String Message { get; set; }
+    }
+}
